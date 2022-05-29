bdb256f92e0cf8bfc27d6f221f862b580be48c4f
kamiteku557 <sereronnrot@gmail.com>
2022-05-30 01:03:34 +0900

    [add] remove file

--- code/scripts/ch1/01_avg.py
+++
@@ -1,5 +0,0 @@
-import ch1
-
-if __name__ == "__main__":
-    ch1.avg_method_1()
-    ch1.avg_method_2()
--- code/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    author="kamiteku557",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)
